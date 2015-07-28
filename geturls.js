9ce669014a1fc4a54e1cb7e179913d97f34c702e
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2015-07-28 13:22:08 +0900

    Update geturls.js

--- geturls.js
+++ geturls.js
@@ -1,21 +1,25 @@
 // リーディングリストから未読のURLを取得する
+var fs = require('fs');
 var plist = require('plist');
 
-var obj = plist.parseFileSync('url.plist');
-//console.log(JSON.stringify(obj));
+var obj = plist.parse(fs.readFileSync('url.plist','utf8'));
+//console.dir(obj);
 
-//lista = data['Children']
-//rl=lista[lista.size-1]['Children']
-//(0..598).each{|i|
-//  puts rl[i]['URIDictionary']['title'] + " " + rl[i]['URLString']
-//}
 var lista = obj.Children;
-var rl = lista[lista.length-1].Children;
-for(var i =0;i<rl.length;i++){
-    if(!rl[i].ReadingList.DateLastViewed) {
-        console.log(/*rl[i].URIDictionary.title + " " + */rl[i].URLString);    
-    } else {
-        //console.log("DL="+rl[i].ReadingList.DateLastViewed);
-    }
-    
+//console.dir(lista);
+console.log("lista.length = "+lista.length);
+var i;
+for(i = 0; i < lista.length; i++) {
+	//console.dir(lista[i].Title);
+	if(lista[i].Title == "com.apple.ReadingList") {
+		break;
+	}
+}
+var rl = lista[i].Children;
+//console.dir(rl);
+
+for(i =0;i<rl.length;i++){
+	if(!rl[i].ReadingList.DateLastViewed) {
+		console.log(rl[i].URIDictionary.title, rl[i].URLString);    
+	}
 }
